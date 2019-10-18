384f6ef29061f7f1800a918f8b21132bdadfbdfc
Dennis Nesanoff <nndennisnn@gmail.com>
2019-10-18 22:44:45 +0300

    Initial Commit

---
+++ Picker/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Picker
+//
+//  Created by Dennis Nesanoff on 17.10.2019.
+//  Copyright © 2019 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    let picker = UIPickerView()
+    let datePicker = UIDatePicker()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        picker.center = view.center
+        datePicker.center = view.center
+        datePicker.datePickerMode = .time
+        
+//        picker.dataSource = self
+//        picker.delegate = self
+        
+        view.addSubview(datePicker)
+        
+        datePicker.addTarget( self, action: #selector(datePickerChanged), for: .valueChanged)
+    }
+    
+    @objc func datePickerChanged() {
+        print("Date is \(datePicker.date)")
+    }
+}
+
+//extension ViewController: UIPickerViewDataSource {
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        return 30
+//    }
+//}
+//
+//extension ViewController: UIPickerViewDelegate {
+//
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        let title = "Row is \(row + 1)"
+//        return title
+//    }
+//}
